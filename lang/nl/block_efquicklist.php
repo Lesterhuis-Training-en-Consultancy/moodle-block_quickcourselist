b788fbd9d5aa44c86133ec5219b2f9bc38169775
LT&C <gemma@ltnc.nl>
2022-03-23 14:32:12 +0100

    NL translation
    Nederlandstalige vertaling

---
+++ lang/nl/block_efquicklist.php
@@ -0,0 +1,46 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * Defines lang strings for Quick Course List
+ *
+ * @package    blocks
+ * @subpackage  efquicklist
+ * @author      Gemma Lesterhuis <gemma@ltnc.nl>
+ * @copyright   2022 Lesterhuis Training en Consultancy, LeendeNL
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+defined('MOODLE_INTERNAL') || die;
+
+$string['blockname'] = 'EF Cursus snelzoeken';
+$string['loading'] = 'Laden...';
+$string['pluginname'] = 'EF Cursus snelzoeken';
+$string['pluginnameplural'] = 'Cursus snelzoek lijsten';
+$string['efquicklist:use'] = 'Gebruik Cursus snelzoeklijst';
+$string['efquicklist'] = 'Cursus snelzoek lijst';
+$string['displaymode'] = 'Resultaat Zoekformat';
+$string['displaymodedescription'] = 'Op welke wijze moeten de resultaten worden getoond.';
+$string['title'] = 'Blok titel';
+$string['splitterms'] = 'Split zoek termen';
+$string['splittermsdescription'] = 'Wanneer ingeschakeld, zoek termen gescheidde tot spaties zullen apart gezocht worden in plaats van als een geheel(bv "Course 101" zal "Course Maths 101" en "Course Science 101" vinden). Inschakelen van deze functie kan leidde tot preformance problemen in Moodle omgevingen met veel cursussen. ';
+$string['restrictcontext'] = 'zoek specifiek op categorie';
+$string['restrictcontextdescription'] = 'Wanneer ingeschakeld, zullen alleen cursussen in de huidige categorie worden getoond';
+$string['coursestartdate'] = 'Startdatum';
+$string['coursecategory'] = 'Categorie';
+$string['efquicklist:myaddinstance'] = 'Voeg blok Cursus snelzoeken toe aan startpagina';
+$string['efquicklist:addinstance'] = 'Voeg blok Cursus snelzoeken toe';
+$string['privacy:metadata'] = 'Het blok Cursus snelzoeken slaat geen persoonlijke data op.';
