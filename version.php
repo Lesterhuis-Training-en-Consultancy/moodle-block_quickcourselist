a818981c5e4726127fee837b72158542391d78ee
LT&C <gemma@ltnc.nl>
2025-04-11 16:48:52 +0200

    Update version.php
    bump version i.v.m. change readme

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2024091000;
+$plugin->version = 2025041100;
 $plugin->requires = 2014051200;
 $plugin->component = 'block_quickcourselist';
 $plugin->maturity = MATURITY_STABLE;
